131281f9ec6fba27b4607d57e8637e05fc0947fd
nimely111 <samuelehnimely@gmail.com>
2024-07-21 00:32:48 +0000

    Convert input value to number and log a random number

--- index.js
+++ index.js
@@ -5,4 +5,9 @@ const result = document.getElementById('result');
 form.addEventListener('submit', e => {
     // stop form from submiting
     e.preventDefault();
+
+       // convert input value from string to number
+const inputValue = parseInt(input.value);
+const randomNum = Math.floor(Math.random() * 6);
+console.log(randomNum);
 })
