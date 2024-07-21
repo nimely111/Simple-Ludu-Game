e38945f664857cf0991b1496e10239c22dfd58e8
nimely111 <samuelehnimely@gmail.com>
2024-07-21 00:27:48 +0000

    Prevent form submission using JavaScript event listener

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const form = document.getElementById('form');
 const input = document.getElementById('input');
 const result = document.getElementById('result');
+
+form.addEventListener('submit', e => {
+    // stop form from submiting
+    e.preventDefault();
+})
